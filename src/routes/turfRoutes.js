f2b4037ed28d98e33b6103322faea90e527e196e
Aarya Agrawal <aarya6105@gmail.com>
2025-07-15 23:54:07 +0530

    Added slots routes

--- src/routes/turfRoutes.js
+++ src/routes/turfRoutes.js
@@ -12,5 +12,10 @@ router.post('/', authMiddleware, turfController.createTurf);
 router.put('/:id', authMiddleware, turfController.updateTurf);
 router.delete('/:id', authMiddleware, turfController.deleteTurf);
 router.patch('/:id/slots', authMiddleware, turfController.updateSlotStatus);
+router.post('/:id/slots', authMiddleware, turfController.addSlots);
+router.delete('/:id/slots', authMiddleware, turfController.deleteSlot);
+router.patch('/:id/slots', authMiddleware, turfController.updateSlot);
+router.patch('/:id/slots', authMiddleware, turfController.updateBulkSlotStatus);
+router.get('/:id/slots', authMiddleware, turfController.getSlots);
 
 module.exports = router;
