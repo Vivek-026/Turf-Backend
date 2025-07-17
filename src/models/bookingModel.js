070147b47d98fd553c987c6995bf14b02d4148d1
vivek-026 <vkbhamare26@gmail.com>
2025-07-17 22:40:58 +0530

    changed booking model

--- src/models/bookingModel.js
+++ src/models/bookingModel.js
@@ -4,9 +4,13 @@ const bookingSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     turf: { type: mongoose.Schema.Types.ObjectId, ref: 'Turf', required: true },
+    court: [{ type: String, required: true }],
+    sport: { type: String, required: true },
     date: { type: Date, required: true },
     day: { type: String, required: true },
-    time: { type: String, required: true },
+    time: { type: String, required: true },     // Start time (e.g., "15:00")
+    endTime: { type: String, required: true },  // End time (e.g., "18:00")
+    hours: { type: Number, required: true },
     pricePaid: { type: Number, required: true },
     status: {
       type: String,
